85ff7e3abe26ab8797c8c79155bded6ad68752a4
silverghost63 <lmbrghn@mail.ru>
2016-05-15 23:32:01 +0400

    change

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo /etc/init.d/nginx start
 ps -o pid,euser,egroup,comm,args -C nginx
-sudo rm /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 ps -o pid,euser,egroup,comm,args -C nginx
