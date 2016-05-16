c140e48ec676f1c8c123d12f1acf6fa9751c848a
silverghost63 <lmbrghn@mail.ru>
2016-05-16 23:35:13 +0400

    change

--- init.sh
+++ init.sh
@@ -5,6 +5,5 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 ps -o pid,euser,egroup,comm,args -C nginx
 
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/gunicorn.conf
-sudo gunicorn -c /etc/gunicorn.d/gunicorn.conf hello:application
-sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart 
