07542946f4ce44c10949301f7e4af0440a0cb2e5
silverghost63 <lmbrghn@mail.ru>
2016-05-22 20:57:41 +0400

    update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo rm -rf /etc/nginx/sites-enabled/test.conf
-sudo ln -s /home/silver/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx start
 ps -o pid,euser,egroup,comm,args -C nginx
