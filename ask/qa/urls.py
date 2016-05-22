8468f4a5783a18d7031a279e081fac4b672afa53
silver <lmbrghn@mail.ru>
2016-05-22 20:51:14 +0400

    Django-application

---
+++ ask/qa/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('qa.views',
+	url(r'^$', 'index', name='index'),
+	url(r'^login/', 'login', name='login'),
+	url(r'^signup/', 'signup', name='signup'),
+	#url(r'^question/(?P<quest_id>[0-9]+)/$', 'question', name='question'),
+	url(r'^question/6384/$', 'question', name='question'),
+	url(r'^ask/', 'ask', name='ask'),
+	url(r'^answer/', 'answer', name='answer'),
+	url(r'^popular/', 'popular', name='popular'),	
+	url(r'^new/', 'test', name='new'),	
+)
--- init.sh
+++ init.sh
@@ -1,9 +1,5 @@
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/silver/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx start
 ps -o pid,euser,egroup,comm,args -C nginx
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
-ps -o pid,euser,egroup,comm,args -C nginx
-
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart 
